bb8245ea99cd757011643f890830baaba64d90ac
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2023-03-05 16:33:53 +0530

    changed indentation to tabs, and added local fonts and functions

--- src/constants.py
+++ src/constants.py
@@ -1,20 +1,25 @@
 # this file contains the locations of the million things that are used in this project.
 
 import os
-from PyQt6 import QtCore
+from PyQt6 import QtCore, QtGui, QtWidgets
 
 # Basics
 UI_WIDTH = 1280
 UI_HEIGHT = 720
 
 # Path related
+
 # this is the puzzlelists directory
 ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
-
-QtCore.QDir.addSearchPath('images', os.path.join(ROOT_DIR, 'resources/images'))
 STYLE_SHEET_PATH = os.path.join(ROOT_DIR, 'src/stylesheet.qss')
 
+# Adding directories to use in the stylesheet
+QtCore.QDir.addSearchPath('images', os.path.join(ROOT_DIR, 'resources/images'))
+QtCore.QDir.addSearchPath('fonts', os.path.join(ROOT_DIR, 'resources/fonts'))
+
 # Game related
+
+# Fonts
 
 
 # Database related
@@ -23,8 +28,3 @@ STYLE_SHEET_PATH = os.path.join(ROOT_DIR, 'src/stylesheet.qss')
 # UI Related
 
 ICON = os.path.join(ROOT_DIR, "resources/logo/icon.svg")
-
-# Stylesheets
-
-background_qss = "background: url('/run/media/krishnaraj/Programs/Python/PuzzleLists/resources/images/login_bg_blur.jpg');\n"
-'color:"#be3114"\n' ""
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from MainWindow import Ui_Puzzlelists
 import os
-import sys
+import sys, time
 
 import game_caller as gc
 import constants as ct
@@ -12,14 +12,19 @@ from PyQt6.QtWidgets import QFileDialog, QListWidget, QMainWindow
 from PyQt6.QtCore import QObject, QThread, pyqtSignal
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
+	app = QtWidgets.QApplication(sys.argv)
 
-    window = Ui_Puzzlelists()
-    
-    with open(ct.STYLE_SHEET_PATH, "r") as f:
-        _style = f.read()
-        app.setStyleSheet(_style)
-    
-    window.show()
+	window = Ui_Puzzlelists()
 
-    app.exec()
+	with open(ct.STYLE_SHEET_PATH, "r") as f:
+		_style = f.read()
+		app.setStyleSheet(_style)
+
+	window.show()
+
+	window.change_Screen(1)
+
+	# for i in range(5):
+	#     window.change_Screen(i)
+	#     time.sleep(1)
+	app.exec()
